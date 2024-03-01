1d0a8a29ea3b00955e0d1089d261ab5f3c3777dd
nikishkaa <nikita.goutor@gmail.com>
2024-03-01 20:32:32 +0300

    Update Magican Astrologer.

--- src/by/itstep/goutor/finaloopproject/model/charecter/MagicianAstrologer.java
+++ src/by/itstep/goutor/finaloopproject/model/charecter/MagicianAstrologer.java
@@ -1,6 +1,6 @@
 package by.itstep.goutor.finaloopproject.model.charecter;
 
-public class MagicianAstrologer {
+public class MagicianAstrologer extends Person {
     private String name;
     private boolean alive;
     private int health; // текущее здоровье
@@ -9,15 +9,20 @@ public class MagicianAstrologer {
 
 
     public MagicianAstrologer(String name, boolean alive, int health, int level, int force) {
-        this.name = name;
-        this.alive = alive;
-        this.health = health;
-        this.level = level;
-        this.force = force;
+        super(name, alive, health, level, force);
     }
 
 
-
+    @Override
+    public String toString() {
+        return "MagicianAstrologer{" +
+                "name='" + name + '\'' +
+                ", alive=" + alive +
+                ", health=" + health +
+                ", level=" + level +
+                ", force=" + force +
+                '}';
+    }
 
 }
 
