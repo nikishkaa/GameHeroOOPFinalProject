a031b747042a1f885911db2359429735065404ce
nikishkaa <nikita.goutor@gmail.com>
2024-03-01 20:39:35 +0300

    Update main class.

--- src/by/itstep/goutor/finaloopproject/controller/Main.java
+++ src/by/itstep/goutor/finaloopproject/controller/Main.java
@@ -1,9 +1,20 @@
 package by.itstep.goutor.finaloopproject.controller;
 
 import by.itstep.goutor.finaloopproject.model.charecter.MagicianAstrologer;
+import by.itstep.goutor.finaloopproject.model.charecter.Person;
 
 public class Main {
     public static void main(String[] args) {
+
+        MagicianAstrologer magicianAstrologer1 =
+                new MagicianAstrologer("1", true, 100, 50, 20);
+
+        MagicianAstrologer magicianAstrologer2 =
+                new MagicianAstrologer("2", true, 100, 40, 10);
+
+
+
+        Person[] persons = {magicianAstrologer1};
 
         System.out.println();
     }
