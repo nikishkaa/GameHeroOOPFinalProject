9e341671145a57c01d633a65322d56f2d619e9a3
nikishkaa <nikita.goutor@gmail.com>
2024-03-02 00:17:07 +0300

    Add Person Manager.

---
+++ src/by/itstep/goutor/finaloopproject/model/logic/PersonManager.java
@@ -0,0 +1,23 @@
+package by.itstep.goutor.finaloopproject.model.logic;
+
+import by.itstep.goutor.finaloopproject.model.charecter.Person;
+
+public class PersonManager {
+    public static Person[] getBiggestLevelPerson(Person[] persons) {
+
+        if (persons == null || persons.length == 0) {
+            return new Person[]{};
+        }
+
+        Person[] level = persons;
+
+        for (int i = 0; i < persons.length; i++) {
+
+
+
+        }
+
+
+        return level;
+    }
+}
