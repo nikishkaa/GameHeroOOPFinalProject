88563c21b03033d68e781797351b27171af6dfde
nikishkaa <nikita.goutor@gmail.com>
2024-03-06 12:55:19 +0300

    Update test  all level architecture.

--- Test/by/itstep/goutor/finaloopproject/model/logic/PersonTest.java
+++ Test/by/itstep/goutor/finaloopproject/model/logic/PersonTest.java
@@ -1,20 +1,30 @@
 package by.itstep.goutor.finaloopproject.model.logic;
+
+import by.itstep.goutor.finaloopproject.model.charecter.MagicianAstrologer;
 import by.itstep.goutor.finaloopproject.model.charecter.Person;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class PersonTest {
     @Test
-    public  void allLevelTest() {
+    public void allLevelTest() {
 
-        int levelOne = 20;
-        int levelTwo = 30;
+        MagicianAstrologer magicianAstrologer1 =
+                new MagicianAstrologer("1", true, 100, 50, 20);
 
-        int expected = 50;
+        MagicianAstrologer magicianAstrologer2 =
+                new MagicianAstrologer("2", true, 100, 40, 10);
 
 
-        Person[] persons = {};
+        Person[] persons = new Person[]{magicianAstrologer1, magicianAstrologer2};
+
+        int expected = 90;
+
         int actual = PersonManager.getAllPersonLevel(persons);
+
+        if (actual != expected) {
+            Assert.fail();
+        }
 
     }
 }
